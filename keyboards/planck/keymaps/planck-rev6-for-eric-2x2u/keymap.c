31892b923f8f0defc96c7c03d00e666a0cf569eb
eric <eric@localhost.localdomain>
2025-08-08 00:01:30 +0300

    Refactor keymap with layer enums and add Windows layout

--- keyboards/planck/keymaps/planck-rev6-for-eric-2x2u/keymap.c
+++ keyboards/planck/keymaps/planck-rev6-for-eric-2x2u/keymap.c
@@ -9,6 +9,15 @@
  * ----------------------------------
  */
 
+// Layer aliases
+enum layer_names {
+    _LINUX_BASE,    // Layer 0 - Linux base layer
+    _WINDOWS_BASE,  // Layer 1 - Windows base layer
+    _SYMBOLS,       // Layer 2 - Symbols and navigation
+    _NUMBERS,       // Layer 3 - Numbers and function keys
+    _FUNCTION,      // Layer 4 - Function keys and RGB controls
+    _MOUSE          // Layer 5 - Mouse layer
+};
 
 const rgblight_segment_t PROGMEM capslock_layer[] = RGBLIGHT_LAYER_SEGMENTS(
     {1, 1, HSV_GREEN},
@@ -43,36 +52,41 @@ bool led_update_user(led_t led_state) {
 }
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-    rgblight_set_layer_state(L_MOUSE, layer_state_cmp(state, 4));
+    rgblight_set_layer_state(L_MOUSE, layer_state_cmp(state, _MOUSE));
     return state;
 }
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	[0] = LAYOUT_planck_2x2u(KC_TAB,  KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I,    KC_O,   KC_P,    KC_BSPC,
+	[_LINUX_BASE] = LAYOUT_planck_2x2u(KC_TAB,  KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I,    KC_O,   KC_P,    KC_BSPC,
                              KC_ESC,  KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K,    KC_L,   KC_SCLN, KC_QUOT,
                              KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, SC_SENT,
-                             KC_LCTL, KC_LGUI, LALT_T(KC_CAPS), KC_LALT,         LT(2, KC_BSPC),     LT(1, KC_SPC),          KC_RALT, KC_APP, KC_RGUI, KC_RCTL),
+                             KC_LCTL, KC_LGUI, LALT_T(KC_CAPS), KC_LALT,         LT(_NUMBERS, KC_BSPC),     LT(_SYMBOLS, KC_SPC),          KC_RALT, KC_APP, KC_RGUI, KC_RCTL),
 
-    [1] = LAYOUT_planck_2x2u(KC_GRV,  KC_NO,   KC_NO,   KC_LCBR, KC_RCBR, KC_NO, KC_NO, KC_PGUP, KC_UP,   KC_PGDN, KC_NO, KC_DEL,
+    [_WINDOWS_BASE] = LAYOUT_planck_2x2u(KC_TAB,  KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I,    KC_O,   KC_P,    KC_BSPC,
+                             KC_ESC,  KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K,    KC_L,   KC_SCLN, KC_QUOT,
+                             KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, SC_SENT,
+                             KC_LCTL, KC_LGUI, LALT(KC_LSFT), KC_LALT,         LT(_NUMBERS, KC_BSPC),     LT(_SYMBOLS, KC_SPC),          KC_RALT, KC_APP, KC_RGUI, KC_RCTL),
+
+    [_SYMBOLS] = LAYOUT_planck_2x2u(KC_GRV,  KC_NO,   KC_NO,   KC_LCBR, KC_RCBR, KC_NO, KC_NO, KC_PGUP, KC_UP,   KC_PGDN, KC_NO, KC_DEL,
                              KC_TRNS, KC_NO,   KC_NO,   KC_LPRN, KC_RPRN, KC_NO, KC_NO, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO, KC_TRNS,
                              KC_TRNS, KC_NO,   KC_NO,   KC_LBRC, KC_RBRC, KC_NO, KC_NO, KC_HOME, KC_NO,   KC_END,  KC_NO, KC_TRNS,
-                             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,              LT(3,KC_BSPC),   KC_TRNS,            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+                             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,              LT(_FUNCTION,KC_BSPC),   KC_TRNS,            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 
-	[2] = LAYOUT_planck_2x2u(KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC,
+	[_NUMBERS] = LAYOUT_planck_2x2u(KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC,
                              KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,
                              KC_TRNS, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,               KC_TRNS,   LT(3, KC_SPC),            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+                             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,               KC_TRNS,   LT(_FUNCTION, KC_SPC),            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 
-	[3] = LAYOUT_planck_2x2u(AS_TOGG, KC_NO,   RGB_VAI, RGB_VAD,  KC_NO, KC_NO, KC_NO, KC_MPRV, KC_VOLU, KC_MNXT, KC_PSCR, KC_NO,
+	[_FUNCTION] = LAYOUT_planck_2x2u(AS_TOGG, KC_NO,   RGB_VAI, RGB_VAD,  KC_NO, KC_NO, KC_NO, KC_MPRV, KC_VOLU, KC_MNXT, KC_PSCR, KC_NO,
                              KC_TRNS, RGB_TOG, RGB_MOD, RGB_RMOD, KC_NO, KC_NO, KC_NO, KC_MPLY, KC_VOLD, KC_MUTE, KC_NO,  KC_TRNS,
-                             KC_TRNS, NK_TOGG, RGB_HUI, RGB_HUD,  KC_NO, KC_NO, KC_NO, TG(4),   KC_NO,   KC_TRNS, KC_TRNS, KC_TRNS,
-                             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS), 
+                             KC_TRNS, NK_TOGG, RGB_HUI, RGB_HUD,  KC_NO, DF(_WINDOWS_BASE), DF(_LINUX_BASE), TG(_MOUSE),   KC_NO,   KC_TRNS, KC_TRNS, KC_TRNS,
+                             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 
-    [4] = LAYOUT_planck_2x2u(KC_TRNS, KC_ACL0, KC_ACL1, KC_ACL2,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_U, KC_TRNS, KC_TRNS, KC_TRNS,
-                             TG(4),   KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS, KC_TRNS,
+    [_MOUSE] = LAYOUT_planck_2x2u(KC_TRNS, KC_ACL0, KC_ACL1, KC_ACL2,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_U, KC_TRNS, KC_TRNS, KC_TRNS,
+                             TG(_MOUSE),   KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS, KC_TRNS,
                              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                KC_BTN2, KC_BTN1,          KC_BTN3, KC_TRNS, KC_TRNS, KC_TRNS)                         
+                             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                KC_BTN2, KC_BTN1,          KC_BTN3, KC_TRNS, KC_TRNS, KC_TRNS)
 };
 
 void keyboard_post_init_user(void) {
